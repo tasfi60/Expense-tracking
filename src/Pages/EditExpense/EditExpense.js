0f41426cc13d0e084583dd40cde2be17be9282f1
tasfi60 <tasfiagirl@gmail.com>
2023-03-05 04:34:13 +0600

    Edit modal updated

--- src/Pages/EditExpense/EditExpense.js
+++ src/Pages/EditExpense/EditExpense.js
@@ -1,6 +1,59 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/Authprovider';
 
 const EditExpense = () => {
+    const{user} = useContext(AuthContext);
+    const [update,setUpdate] = useState([]);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/expense/${user.uid}`;
+    
+        fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => setUpdate(data))
+          .catch(error => console.error(error));
+      }, [user.uid]);
+
+
+    const [ newExpense, setNewExpense] = useState(update);
+
+    const handleInputChange = event =>{
+       const field = event.target.name;
+       const value = event.target.value;
+       const newReview = {...newExpense}
+       newReview[field] = value;
+       setNewExpense(newReview);
+
+    }
+
+    const handleUpdateReview = event =>{
+       event.preventDefault();
+       console.log(newExpense);
+       fetch(`http://localhost:5000/expense/${user.uid}`,{
+             method: 'PUT',
+             headers:{
+               'content-type': 'application/json'
+             },
+             body: JSON.stringify(newExpense)
+       })
+       .then(res => res.json())
+       .then(data =>{
+           if(data.modifiedCount > 0){
+               alert('Review Updated!')
+               console.log(data);
+               event.target.reset();
+           }
+              
+       })
+   }
+       
+
+
     return (
         <div>
            
@@ -17,9 +70,9 @@ const EditExpense = () => {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <form class="font-style">
+      <form onSubmit={handleUpdateReview} class="font-style">
           <label for="option" class="grid-whole padded fs-4 font-style fw-bold me-4">Expense Category:</label>
-          <select id="option" class="p-1" name="category">
+          <select id="option" class="p-1" name="category" onChange={handleInputChange} defaultValue={update.Category}>
             <option value="Housing and utilities">Housing and utilities</option>
             <option value="Grocery">Grocery</option>
             <option value="Transportation">Transportation</option>
@@ -35,14 +88,16 @@ const EditExpense = () => {
           </select>
           <br/>
           <label for="option" class="grid-whole padded fs-4 font-style fw-bold me-4">Price:</label>
-          <select id="currency" name="currency">
+          <select id="currency" name="currency" onChange={handleInputChange}
+          defaultValue={update.Currency}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="INR">INR</option>
           <option value="GBP">GBP</option>
           <option value="BDT">BDT</option>
         </select>
-        <input type="number" id="price" name="price" min="1.00" step="1" class="w-50 h-25 ms-3 border border-dark" required/>
+        <input type="number" id="price" name="price" onChange={handleInputChange} 
+        defaultValue={update.Price} min="1.00" step="1" class="w-50 h-25 ms-3 border border-dark" required/>
           <br/>
 
         
