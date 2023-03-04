1ef9be0218194af4551278a4e28f2e6ac9986081
tasfi60 <tasfiagirl@gmail.com>
2023-03-05 04:13:50 +0600

    Edit operation added

--- src/Pages/EditExpense/EditExpense.js
+++ src/Pages/EditExpense/EditExpense.js
@@ -13,15 +13,47 @@ const EditExpense = () => {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Edit here</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+      <form class="font-style">
+          <label for="option" class="grid-whole padded fs-4 font-style fw-bold me-4">Expense Category:</label>
+          <select id="option" class="p-1" name="category">
+            <option value="Housing and utilities">Housing and utilities</option>
+            <option value="Grocery">Grocery</option>
+            <option value="Transportation">Transportation</option>
+            <option value="Personal care">Personal care</option>
+            <option value="Health and fitness">Health and fitness</option>
+            <option value="Education">Education </option>
+            <option value="Gifts and donations">Gifts and donations</option>
+            <option value="Travel">Travel </option>
+            <option value="Insurance">Insurance</option>
+            <option value="Taxes">Taxes</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Other">Other</option>
+          </select>
+          <br/>
+          <label for="option" class="grid-whole padded fs-4 font-style fw-bold me-4">Price:</label>
+          <select id="currency" name="currency">
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="INR">INR</option>
+          <option value="GBP">GBP</option>
+          <option value="BDT">BDT</option>
+        </select>
+        <input type="number" id="price" name="price" min="1.00" step="1" class="w-50 h-25 ms-3 border border-dark" required/>
+          <br/>
+
+        
+
+        <br/>
+        <button type="submit" class="form-button btn mt-3 btn-md margin-b w-50 fs-6 rounded fs-6"><small>Save changes</small></button>
+        </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="form-button btn mt-3 btn-md margin-b w-25 fs-6 rounded" data-bs-dismiss="modal">Close</button>
+       
       </div>
     </div>
   </div>
