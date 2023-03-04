b361440cc6139e3493c7f79eb36b36f39870431a
tasfi60 <tasfiagirl@gmail.com>
2023-03-05 04:01:10 +0600

    summary added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EditExpense from '../EditExpense/EditExpense';
 
 const Dashboard = () => {
+
+
+    const [expense,setExpense] = useState([])
+
+    fetch('http://localhost:5000/expense',{
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json'
+                }
+                
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setExpense(data)
+                    
+                })
+                .catch(error => console.error(error));
+
+    
     return (
-        <div>
+        <div class="font-style my-4">
+            <h3>Expense & Budget summary</h3>
             <table class="table table-success table-striped">
+                
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">No</th>
+      <th scope="col">Budget</th>
+      <th scope="col">Expense Category</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    
+        { 
+          expense.map((e,i) => <tr>
+            <th scope="row">{i + 1}</th>
+            <td>{e.Price} {e.Currency}</td>
+            <td>{e.Category}</td>
+            <td><EditExpense></EditExpense></td>
+            <td><EditExpense></EditExpense></td>
+          </tr>)
+          
+
+        }
+      
+    
+  
   </tbody>
 </table>
         </div>
